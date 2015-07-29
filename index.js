016aec2dc51925767ea05f355f267dfe7a11ab61
Adam Veary <adamjv90@gmail.com>
2015-07-29 09:51:41 -0400

    script

---
+++ index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+var fs = require('fs');
+var _ = require('lodash');
+var Table = require('cli-table');
+var figures = require('figures');
+var algoliasearch = require('algoliasearch');
+
+var each = _.each;
+var filter = _.filter;
+var map = _.map;
+
+var client = algoliasearch('MI4MSOKC78', '20dc1cdfee4ce4a4573f2a648ea97d79');
+var index = client.initIndex('images');
+
+var table = new Table({
+    head: ['ID', 'Date', 'LBB', 'Tags 3', 'Tags 2', 'Tag', 'ETags A', 'ETags B'],
+    colWidths: [9, 8, 5, 8, 8, 7, 10, 10]
+});
+
+function creditsContainLBB (credits) {
+    return filter(credits, function (credit) {
+        return credit.isLbb;
+    }).length;
+}
+
+function tagsMatchingQuery (tags) {
+    return filter(map(tags, function (tag) {
+        switch (tag.matchLevel) {
+            case 'full':
+                return figures.circleFilled;
+                break;
+            case 'partial':
+                return  figures.circleDotted;
+                break;
+            default:
+                return false;
+        }
+    }));
+}
+
+index.search(process.argv[2], function searchDone(err, contents) {
+    each(contents.hits, function (row) {
+        table.push([row.objectID, row.ym.substring(4, 6) + '/' + row.ym.substring(2, 4), creditsContainLBB(row.credits) ? figures.tick : figures.cross, tagsMatchingQuery(row._highlightResult.tags3).join(''), tagsMatchingQuery(row._highlightResult.tags2).join(''), tagsMatchingQuery(row._highlightResult.tags1).join(''), tagsMatchingQuery(row._highlightResult.etagsA).join(''), tagsMatchingQuery(row._highlightResult.etagsB).join('')]);
+    });
+
+    console.log(table.toString());
+});
