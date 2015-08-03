a62041cee2b5f4910f02a2c3b11bcfcd13db32d8
Satya <lprajus2007@gmail.com>
2015-08-03 10:44:55 -0400

    Added isImageLbb

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ var client = algoliasearch('MI4MSOKC78', '20dc1cdfee4ce4a4573f2a648ea97d79');
 var index = client.initIndex('images');
 
 var table = new Table({
-    head: ['ID', 'Date', 'GID', 'LBB', 'Tags 3', 'Tags 2', 'Tag', 'ETags A', 'ETags B'],
-    colWidths: [9, 8, 7, 5, 8, 8, 7, 9, 9]
+    head: ['ID', 'Date', 'GID', 'isImageLbb', 'Tags 3', 'Tags 2', 'Tag', 'ETags A', 'ETags B'],
+    colWidths: [9, 8, 7, 8, 8, 8, 7, 9, 9]
 });
 
-function creditsContainLBB (credits) {
-    return filter(credits, function (credit) {
-        return credit.isLbb;
-    }).length;
-}
+// function creditsContainLBB (credits) {
+//     return filter(credits, function (credit) {
+//         return credit.isLbb;
+//     }).length;
+// }
 
 function tagsMatchingQuery (tags) {
     return filter(map(tags, function (tag) {
@@ -53,7 +53,7 @@ index.search(process.argv[2], {
     ]
 }, function searchDone(err, contents) {
     each(contents.hits, function (row) {
-        table.push([row.objectID, row.ym.substring(4, 6) + '/' + row.ym.substring(2, 4), row.gid, creditsContainLBB(row.credits) ? figures.tick : figures.cross, tagsMatchingQuery(row._highlightResult.tags3).join(''), tagsMatchingQuery(row._highlightResult.tags2).join(''), tagsMatchingQuery(row._highlightResult.tags1).join(''), tagsMatchingQuery(row._highlightResult.etagsA).join(''), tagsMatchingQuery(row._highlightResult.etagsB).join('')]);
+        table.push([row.objectID, row.ym.substring(4, 6) + '/' + row.ym.substring(2, 4), row.gid, row.isImageLbb ? figures.tick : figures.cross, tagsMatchingQuery(row._highlightResult.tags3).join(''), tagsMatchingQuery(row._highlightResult.tags2).join(''), tagsMatchingQuery(row._highlightResult.tags1).join(''), tagsMatchingQuery(row._highlightResult.etagsA).join(''), tagsMatchingQuery(row._highlightResult.etagsB).join('')]);
     });
 
     console.log(table.toString());
